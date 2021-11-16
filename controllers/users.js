45e8561c2c530f6341613044052eaee52fcf5237
momo <mh.zeghdoudi@gmail.com>
2021-11-16 12:07:08 +0100

    adding controller example with mariadb

--- controllers/users.js
+++ controllers/users.js
@@ -15,5 +15,18 @@ module.exports = {
             return res.status(400).send("erreur example2")
         }
         return res.status(200).send('ok')
+    },
+    exampleWithDB: async (req, res, next) => {
+        let connexion;
+        try {
+            connexion = await pool.getConnexion();
+            const result = await connexion.query('CALL ma_procedure');
+            console.log(result);
+            return res.status(200).json({ success: result })
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        } finally {
+            if (conn) conn.end()
+        }
     }
 }
