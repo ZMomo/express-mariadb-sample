79b64bc278c56b98ba376e3a14d2d8f276c202b0
ZMomo <mh.zeghdoudi@gmail.com>
2022-01-27 13:35:00 +0100

    Creation d' un controlleur permettant de poster une todo

--- controllers/users.js
+++ controllers/users.js
@@ -16,11 +16,26 @@ module.exports = {
         }
         return res.status(200).send('ok')
     },
-    exampleWithDB: async (req, res, next) => {
+    exampleWithDB: async (req, res) => {
         let connexion;
         try {
             connexion = await pool.getConnection();
             const result = await connexion.query('SELECT * FROM todo;');
+            console.log(result);
+            return res.status(200).json({ success: result })
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        } finally {
+            if (connexion) connexion.end()
+        }
+    },
+    createTodo: async (req, res) => {
+        const { text } = req.body;
+        // const text = req.body.text
+        let connexion;
+        try {
+            connexion = await pool.getConnection();
+            const result = await connexion.query('INSERT into todo (`text`) VALUES (?);', [text]);
             console.log(result);
             return res.status(200).json({ success: result })
         } catch (error) {
